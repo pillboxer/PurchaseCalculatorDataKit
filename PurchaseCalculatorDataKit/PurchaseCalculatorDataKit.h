4759c00e7b25c061e2d55ec2198ab9be9628345a
Henry Cooper <henry@touchnote.com>
2020-12-17 09:05:08 +0000

    Initial Commit

---
+++ PurchaseCalculatorDataKit/PurchaseCalculatorDataKit.h
@@ -0,0 +1,18 @@
+//
+//  PurchaseCalculatorDataKit.h
+//  PurchaseCalculatorDataKit
+//
+//  Created by Henry Cooper on 17/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PurchaseCalculatorDataKit.
+FOUNDATION_EXPORT double PurchaseCalculatorDataKitVersionNumber;
+
+//! Project version string for PurchaseCalculatorDataKit.
+FOUNDATION_EXPORT const unsigned char PurchaseCalculatorDataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PurchaseCalculatorDataKit/PublicHeader.h>
+
+
